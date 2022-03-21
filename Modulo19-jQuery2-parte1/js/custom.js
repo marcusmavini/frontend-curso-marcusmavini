f9f54ad4811180e495bc4a86274c1ef63c94da1c
Marcus Mavini <marcus.mavini9@gmail.com>
2022-03-21 20:19:19 -0300

    adiciona mais uma pasta de treinamento e modulo19

--- Modulo19-jQuery2-parte1/js/custom.js
+++ Modulo19-jQuery2-parte1/js/custom.js
@@ -52,4 +52,87 @@ $(document).ready(function () {
       );
     }
   });
+
+  /*Ouvinte de eventos .nav-modal-open*/
+  $('.nav-modal-open').on('click', function(e){
+    e.preventDefault();
+
+    let elem = $(this).attr('rel');
+
+    $('.modal-body').html($('#'+ elem).html());
+    $('.modal-header h5.modal-title').html($(this).text());
+
+    let myModal = new bootstrap.Modal($('#modelId'));
+
+    myModal.show();
+  })
+
+  function validate(elem){
+    if(elem.val() == ''){
+        console.log('O campo '+ elem.attr('name') + ' é obrigatório');
+        elem.parent().find('.text-muted').show()
+        elem.addClass('invalid');
+        return false;
+      }else{
+        elem.parent().find('.text-muted').hide()
+        elem.removeClass('invalid');
+      }
+  }
+
+  $('body').on('submit', '.modal-body .form',function(e){
+    e.preventDefault();
+
+    const inputName = $('#nome');
+    const inputEmail = $('#email');
+
+    validate(inputName);
+    validate(inputEmail);
+
+    if(inputEmail.hasClass('invalid') || inputName.hasClass('invalid')){
+      console.log('Verificar os campos obrigatórios');
+      return false;      
+    }else{
+      $(this).submit();
+    }
+  
+  })
+
+  $('body').on('blur', '#nome', function(e){
+    validate($(this));
+  })
+
+  $('body').on('blur', '#email', function(e){
+    validate($(this));
+  })
+
+$('body').on('focus', '#date', function(){
+  $(this).datepicker();
+})
+
+  $('body').on('blur', '#date', function(e){
+    validate($(this));
+    $('#date').mask('00/00/0000');
+  })
+
+  $('body').on('blur', '#time', function(e){
+    validate($(this));
+    $('#time').mask('00:00');
+  })
+
+  $('body').on('blur', '#cep', function(e){
+    validate($(this));
+    $('#cep').mask('00000-000');
+  })
+
+  $('body').on('blur', '#phone', function(e){
+    validate($(this));
+    $('#phone').mask('(00)00000-0000');
+  })
+
+  $('body').on('blur', '#cpf', function(e){
+    validate($(this));
+    $('#cpf').mask('000.000.000-00', {reverse: true});
+  }) 
+  
+
 });
